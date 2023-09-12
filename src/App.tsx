047db67eb15690b77410062ec2255b199c94f05e
Adammanandhar <aayush.manandhar17@gmail.com>
2023-09-12 19:19:13 -0400

    added info in the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1>My personal app</h1>
             <header className="App-header">
+                <img
+                    src="https://media.licdn.com/dms/image/C4E03AQHx03oyKy9IWg/profile-displayphoto-shrink_800_800/0/1625844227180?e=2147483647&v=beta&t=IcutS7mUL28K7sOMQ4STn5SOto9gAyw6YTDvdfPzRnU"
+                    alt="A picture of my dog Ada"
+                />
+                My hobbiest:
+                <ul>
+                    <li>Watch Soccer</li>
+                    <li>Hike</li>
+                    <li>Sleep</li>
+                </ul>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+                <Container>
+                    <Row>
+                        <Col>Aayush</Col>
+
+                        <Col>Manandhar</Col>
+                    </Row>
+                </Container>
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
