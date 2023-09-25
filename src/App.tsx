03566a658465076f8121d497348953ca8001331d
Aayush Manandhar <34245763+Adammanandhar@users.noreply.github.com>
2023-09-25 15:02:15 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <h1>COS420</h1>
+            <h1>my app</h1>
             <header className="App-header">
                 <img
                     src="https://media.licdn.com/dms/image/C4E03AQHx03oyKy9IWg/profile-displayphoto-shrink_800_800/0/1625844227180?e=2147483647&v=beta&t=IcutS7mUL28K7sOMQ4STn5SOto9gAyw6YTDvdfPzRnU"
@@ -27,7 +27,7 @@ function App(): JSX.Element {
                         <Col>Manandhar</Col>
                     </Row>
                 </Container>
-                UD CISC275 with React Hooks and TypeScript
+                UM COS420 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
